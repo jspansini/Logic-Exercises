ed580f25154bc1f445728227ca6859c0f0311c47
jessica pansini <jspansini@gmail.com>
2023-01-10 01:15:54 -0300

    Implentação exercício caneta

---
+++ 001 - Caneta/001 - Caneta/Models/Caneta.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001___Caneta.Models
+{
+    public enum ModeloCaneta
+    {
+        Bic,
+        Compactor,
+        MarcaCis           
+    }
+    public enum TiposDePonta
+    {
+        PontaGrossa,
+        PontaMedia,
+        PontaFina
+    }
+    public class Caneta
+
+    {
+        public int Codigo { get; set; }
+        public ModeloCaneta Modelo { get; set; }
+        public string Cor { get; set; }
+        public TiposDePonta Ponta { get; set; }
+        public bool Tampada { get; set; }
+        public double Preco { get; set; }
+        public Caneta(int codigo, ModeloCaneta modelo, string cor,TiposDePonta ponta, bool tampada, double preco)
+        {
+            this.Codigo = codigo;
+            this.Modelo = modelo;
+            this.Cor = cor;
+            this.Tampada = tampada;
+            this.Preco = preco;
+            this.Ponta = ponta;
+        }
+
+        public Caneta()
+        {
+        }
+
+        public string Rabiscar()
+        {
+            if (Ponta.Equals(TiposDePonta.PontaGrossa)){
+                return "Toda vez que você chama eu rabisco algo SAI GROOOOOOOSSSO";
+            }else if (Ponta.Equals(TiposDePonta.PontaMedia))
+            {
+                return "Toda vez que você chama eu rabisco algo SAI MEDIOOOOOO";
+            }
+            else
+            {
+                return "Toda vez que você chama eu rabisco algo SAI FIIIIIIIIIIIIINO";
+            }
+        }
+
+        public string Escrever(string mandaAlgo)
+        {
+            if(mandaAlgo != null)
+            {
+                return $"Taí o que você queria escrito em tela: {mandaAlgo}";
+            }
+            else
+            {
+                return "erro!";
+            }
+        }
+
+        public string TamparOuDestampar()
+        {
+            this.Tampada = Tampada;
+            if(!Tampada)
+            {
+                Tampada = true;
+                return "Agora você fechou!";
+            }
+            else
+            {
+                Tampada = false;
+                return "Agora você abriu!";
+            }
+        }
+    }
+}
---
+++ 001 - Caneta/001 - Caneta/Program.cs
@@ -0,0 +1,35 @@
+﻿using _001___Caneta.Models;
+
+//instancia usando construtor vazio
+Caneta caneta01 = new Caneta();
+caneta01.Cor = "Vermelho";
+caneta01.Codigo = 131313;
+caneta01.Preco = 8.13;
+caneta01.Tampada = true;
+string msg = caneta01.Escrever("Uma mensagem linda!");
+
+Console.WriteLine("\n---------------------------------\n");
+
+Console.WriteLine("Essa é a primeira caneta\n");
+Console.WriteLine("A cor é: " + caneta01.Cor);
+Console.WriteLine("O código é " + caneta01.Codigo);
+Console.WriteLine("A caneta está tampada?" + caneta01.Tampada);
+Console.WriteLine("O preço da caneta é " + caneta01.Preco);
+Console.WriteLine(msg);
+
+Console.WriteLine("\n---------------------------------\n");
+
+//instancia usando  construtor
+
+Caneta caneta02= new Caneta(15, ModeloCaneta.Bic, "Rosa",TiposDePonta.PontaGrossa, false, 115.00);
+Console.WriteLine("Essa é a segunda  caneta:\n");
+Console.WriteLine(caneta02.Tampada);
+Console.WriteLine(caneta02.Codigo);
+Console.WriteLine(caneta02.Modelo);
+Console.WriteLine(caneta02.Cor);
+Console.WriteLine(caneta02.TamparOuDestampar());
+Console.WriteLine(caneta02.Rabiscar());
+Console.WriteLine(caneta02.Escrever("queremos um pacote de biscoitos!"));
+Console.WriteLine("\n---------------------------------\n");
+
+
