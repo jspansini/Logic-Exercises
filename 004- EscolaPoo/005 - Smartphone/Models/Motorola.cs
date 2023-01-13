1571309736ab4274f41577e4b196a50df2be5f99
jessica pansini <jspansini@gmail.com>
2023-01-13 16:31:01 -0300

    smartphne

---
+++ 004- EscolaPoo/005 - Smartphone/Models/Motorola.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005___Smartphone.Models
+{
+    public class Motorola : Smartphone
+    {
+        public Motorola(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Estou instalando o {nomeApp} no meu Motorola via PlayStore!");
+        }
+    }
+}
---
+++ 004- EscolaPoo/005 - Smartphone/Models/Smartphone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005___Smartphone.Models
+{
+    public abstract class Smartphone
+    {
+            public string Numero { get; set; }
+            public string Modelo { get; set; }
+            public string Imei { get; set; }
+            public int Memoria { get; set; }
+
+            public Smartphone(string numero, string modelo, string imei, int memoria)
+            {
+                this.Numero = numero;
+                this.Modelo = modelo;
+                this.Imei = imei;
+                this.Memoria = memoria;
+            }
+
+            public void Ligar()
+            {
+                Console.WriteLine("Ligando...");
+            }
+
+            public void ReceberLigacao()
+            {
+                Console.WriteLine("Recebendo ligação...");
+            }
+
+            public abstract void InstalarAplicativo(string nomeApp);
+        
+    }
+}
