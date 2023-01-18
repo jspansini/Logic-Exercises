0487b8da197a5e59ea87c3f8a0df5f6b16bc7a82
jessica pansini <jspansini@gmail.com>
2023-01-18 08:01:09 -0300

    reflection

---
+++ 006 - Reflection/ExercicioReflection/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioReflection;
+using System.Reflection;
+
+//crie um método chamado DisplayPublicProperties Exibindo as classes públicas
+//GetType faz esse trabalho naturalmente exibindo as classes publicas, adicionei uma prop id private pra testar!
+void DisplayPublicProperties(object classe)
+{
+    Type type = classe.GetType();
+    Console.WriteLine($"Imprimindo as informações da classe {classe}");
+    foreach (PropertyInfo prop in type.GetProperties())
+    {
+        Console.WriteLine(prop.Name);
+    }
+}
+
+Console.WriteLine();
+
+//instanciando um student
+Student student = new Student();
+Console.WriteLine("Propriedades públicas da classe:");
+DisplayPublicProperties(student);
+
+Console.WriteLine();
+
+
+//Use Reflection para preencher as propriedades públicas do objeto.
+
+Type type = student.GetType();
+
+object PropManual = Activator.CreateInstance(type);
+
+PropertyInfo propNome = type.GetProperty("Name");
+propNome.SetValue(PropManual, "Jess Pansini");
+
+PropertyInfo propUniversidade = type.GetProperty("University");
+propUniversidade.SetValue(PropManual, "42 Rio");
+
+PropertyInfo propRollNumber = type.GetProperty("RollNumber");
+propRollNumber.SetValue(PropManual, 132313);
+
+PropertyInfo propIdStudent = type.GetProperty("IdStudent");
+propRollNumber.SetValue(PropManual, 13);
+
+
+//Use Reflection para chamar o método DisplayInfo do objeto criado
+
+Console.WriteLine("****************************");
+MethodInfo mostrarMetodo = type.GetMethod("DisplayInfo");
+object result = mostrarMetodo.Invoke(PropManual, null);
---
+++ 006 - Reflection/ExercicioReflection/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioReflection
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+        public int IdStudent { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Esse metodo você está vendo artavés da minha classe Student: {Name} - {University} - {RollNumber} - {IdStudent}");
+        }
+    }
+}
